ef6883ce953ba544c793b32c3b779bacecb6c46e
Richard Zhao <09zwcbupt@gmail.com>
2014-04-28 15:54:39 -0700

    fix problem on server

--- qemu-ifup.sh
+++ qemu-ifup.sh
@@ -2,13 +2,13 @@
 set -x
 
 switch=br0
-ovs-vsctl del-port br0 $1
+sudo ovs-vsctl del-port br0 $1
 
 if [ -n "$1" ];then
-        /usr/bin/sudo /usr/sbin/tunctl -u `whoami` -t $1
+        sudo /usr/bin/sudo /usr/sbin/tunctl -u `whoami` -t $1
         /usr/bin/sudo /sbin/ip link set $1 up
-        sleep 0.5s
-        /usr/bin/sudo ovs-vsctl add-port $switch $1
+        sleep 0.1s
+        sudo /usr/bin/sudo ovs-vsctl add-port $switch $1
         exit 0
 else
         echo "Error: no interface specified"
